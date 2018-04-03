309082d6d69659e0997a33e4225f9cb2d6e07406
Erick Arroyo <erick.1726@gmail.com>
2018-04-03 11:16:43 -0500

    Updating close button image

--- imagemodal/src/main/java/com/erickarroyo/imagemodal/ImageModal.java
+++ imagemodal/src/main/java/com/erickarroyo/imagemodal/ImageModal.java
@@ -17,123 +17,42 @@ public class ImageModal {
     Context context;
 
     public Dialog builder;
+    public ImageButton imageButton;
+    public ImageView imageView;
 
     public ImageModal(Context context) {
         this.context = context;
     }
 
     public void showModal(String url) {
-        final Dialog builder = new Dialog(context);
-        builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        builder.getWindow().setBackgroundDrawable(
-                new ColorDrawable(android.graphics.Color.TRANSPARENT));
-
-        builder.setContentView(R.layout.dialog_layout);
-
-
-        ImageButton imageButton = (ImageButton) builder.findViewById(R.id.close_modal_btn);
-        ImageView imageView = (ImageView) builder.findViewById(R.id.image_view);
-
-
-        imageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                builder.dismiss();
-
-            }
-        });
-
-        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                Log.d("MODAL_CLOSED", "modal_closed");
-            }
-        });
-
-        Picasso.get().load(url).into(imageView);
-
-
+        setup(url);
         builder.show();
-
-
     }
 
+
     public void showModal(String url, DialogInterface.OnDismissListener listener) {
+        setup(url);
+        builder.setOnDismissListener(listener);
+        builder.show();
+    }
+
+    private void setup(String url) {
         builder = new Dialog(context);
         builder.requestWindowFeature(Window.FEATURE_NO_TITLE);
         builder.getWindow().setBackgroundDrawable(
                 new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
         builder.setContentView(R.layout.dialog_layout);
-
-
-        ImageButton imageButton = (ImageButton) builder.findViewById(R.id.close_modal_btn);
-        ImageView imageView = (ImageView) builder.findViewById(R.id.image_view);
-
-
+        imageButton = (ImageButton) builder.findViewById(R.id.close_modal_btn);
+        imageView = (ImageView) builder.findViewById(R.id.image_view);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 builder.dismiss();
+
             }
         });
 
-        builder.setOnDismissListener(listener);
-
         Picasso.get().load(url).into(imageView);
-
-
-
-
-
-
-
-        /*
-
-
-
-        builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialogInterface) {
-                //nothing;
-            }
-        });
-
-
-
-
-
-        ImageButton imageButton = new ImageButton(this);
-        imageButton.setImageResource(R.mipmap.ic_close_modal);
-        imageButton.setBackgroundColor(Color.parseColor("#00000000"));
-
-        builder.addContentView(imageButton, new RelativeLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-
-
-        ImageView imageView = new ImageView(this);
-
-
-        //Uri imgUri = Uri.parse("android.resource://com.erickarroyo.testmodal/" + R.drawable.ic_novedades_test);
-        //imageView.setImageURI(null);
-        //imageView.setImageURI(imgUri);
-
-
-        builder.addContentView(imageView, new RelativeLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT));
-
-
-
-        Picasso.get().load(url).into(imageView);
-
-
-
-*/
-
-        builder.show();
-
-
     }
 }
